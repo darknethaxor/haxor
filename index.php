e5061f5bb06392aebb05b27360c3efea459d5093
darknethaxor <72026007+darknethaxor@users.noreply.github.com>
2023-11-01 02:01:38 +0600

    Update index.php

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
-<head><meta property="og:site_name" content=""><meta property="og:title" content=""><meta property="og:image" content=""><meta property="og:description" content=""></head>
+<head></head>
 <body>
 <script>
 bc = <?php echo json_encode(basename($_SERVER['PHP_SELF']) ?? null) ?>;
